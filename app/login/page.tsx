2e498d95cd2862ac74864542dc3269c3b7d99fa2
Saularch97 <duart.herrera@gmail.com>
2024-09-26 01:41:49 -0300

    base form and login route created

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import { Form } from "@/components/ui/form";
+import { TicketForm } from "@/components/ui/ticket_form";
+
+ export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+        <TicketForm></TicketForm>
+    </main>
+  );
+}
---
+++ components/ui/ticket_form.tsx
@@ -0,0 +1,74 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./form"
+import { Button } from "./button"
+import { Input } from "./input"
+
+const formSchema = z.object({
+    username: z.string().min(5, {
+        message: "Username must be at least 5 characters.",
+    }),
+    password: z.string().min(8, {
+        message: "Password must be at least 8 charactres."
+    })
+})
+
+export function TicketForm() {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: ""
+        }
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" {...field} type="password"/>
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      )
+}
